2d7665744aee74e4cc76ff900774171a64b404f2
Cole Anthony <cole.edwin.anthony@gmail.com>
2022-11-17 12:09:19 -0700

    make riddle app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [riddle, setRiddle] = useState({})
+
+  useEffect(() => {
+  const options = {
+	method: 'GET',
+	headers: {
+		'X-API-Key': 'Pz5s/PcmYDE3PqSzPpfvNA==Ome2rxMJlOQOmk81',
+		
+	}
+};
+
+fetch('https://api.api-ninjas.com/v1/riddles', options)
+	.then(response => response.json())
+	.then(response => setRiddle(response[0]))
+	.catch(err => console.error(err));
+  }, [])
+
+const newJoke = (event) => {
+  event.preventDefault()
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-API-Key': 'Pz5s/PcmYDE3PqSzPpfvNA==Ome2rxMJlOQOmk81',
+      
+    }
+  };
+  
+  fetch('https://api.api-ninjas.com/v1/riddles', options)
+    .then(response => response.json())
+    .then(response => setRiddle(response[0]))
+    .catch(err => console.error(err));
+}  
+
+return (
+  <div className='App'>
+    <h3 className='title'>riddledogz</h3>
+    <div className='riddle-container'>
+    <h2>{riddle.question}</h2>
+    <h3>{riddle.answer}</h3>
+    <button className='button-17' onClick={newJoke}>RIDDLE ME AGAIN, BITCH!</button>
     </div>
-  );
+    </div>
+)
 }
 
 export default App;
